7179c783522550bde5f1ba5340bee6b3e62cb042
veok <p.lelental@gmail.com>
2018-12-16 16:08:21 +0100

    Hello world

--- app/src/main/java/com/example/trebb/currencyexchange/CalculatorActivity.kt
+++ app/src/main/java/com/example/trebb/currencyexchange/CalculatorActivity.kt
@@ -11,12 +11,7 @@ class CalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
 }
