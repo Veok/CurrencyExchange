5964b1b9fffec771bc913b05ff6da551eee33b76
veok <p.lelental@gmail.com>
2018-12-16 15:25:40 +0100

    Refactoring

--- app/src/main/java/com/example/trebb/currencyexchange/MainActivity.kt
+++ app/src/main/java/com/example/trebb/currencyexchange/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.trebb.currencyexchange
 
-import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
 import android.widget.Toast
 import com.example.trebb.currencyexchange.service.CurrencyService
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -10,7 +12,8 @@ import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), CurrencyInfo.OnFragmentInteractionListener {
+
+class MainActivity : AppCompatActivity() {
 
     private var disposable: Disposable? = null
 
@@ -22,12 +25,44 @@ class MainActivity : AppCompatActivity(), CurrencyInfo.OnFragmentInteractionList
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn_search.setOnClickListener {
-            beginSearch()
+        }
+
+        currencyChangeListener()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disposable?.dispose()
+    }
+
+
+    /**
+     * Function that listens a spinner selection. After change it invokes methods to change country flag image
+     * and search current currency from API
+     */
+    private fun currencyChangeListener() {
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                changeCountryFlag(p0)
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                changeCountryFlag(p0)
+                beginSearch()
+            }
         }
     }
 
-    override fun onFragmentInteraction(uri: Uri) {
+    /**
+     * Function that takes fragment instance and sets proper country flag image
+     * depending an user selection of a spinner
+     */
+    private fun changeCountryFlag(p0: AdapterView<*>?) {
+        val fm = supportFragmentManager
+        val fragment = fm.findFragmentById(R.id.fragment2) as CurrencyInfo
+        fragment.changeFlag(p0?.selectedItem.toString())
     }
+
 
     /**
      * Function that uses CurrencyService to connect with API and to get current exchange rates.
@@ -35,21 +70,18 @@ class MainActivity : AppCompatActivity(), CurrencyInfo.OnFragmentInteractionList
      * If not, shows an error message.
      */
     fun beginSearch() {
-        disposable = currencyService.getCurrency("usd")
+        val mySpinner = findViewById<Spinner>(R.id.spinner)
+        disposable = currencyService.getCurrency(mySpinner.selectedItem.toString())
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({ result ->
-                sell_result.text = "${result.rates[0].ask}"
-                buy_result.text = "${result.rates[0].bid}"
+                sell_result.text = "${result.rates[0].ask} PLN"
+                buy_result.text = "${result.rates[0].bid} PLN"
             },
                 {
                     Toast.makeText(this, R.string.errorWithConnection, Toast.LENGTH_SHORT).show()
                 })
     }
 
-    override fun onPause() {
-        super.onPause()
-        disposable?.dispose()
-    }
 
 }
--- app/src/main/java/com/example/trebb/currencyexchange/service/CurrencyService.kt
+++ app/src/main/java/com/example/trebb/currencyexchange/service/CurrencyService.kt
@@ -1,22 +1,28 @@
 package com.example.trebb.currencyexchange.service
 
 import com.example.trebb.currencyexchange.model.Currency
+import io.reactivex.Observable
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Query
-import java.util.*
-import io.reactivex.Observable
 import retrofit2.http.Path
-import retrofit2.http.Url
 
-
+/**
+ * Service used to retrieve current currency info from NBP API
+ */
 interface CurrencyService {
 
+    /**
+     * Function that retrieves current currency info from NBP API.
+     * @param currency - a country code that is passed to get currency of specified country
+     */
     @GET("exchangerates/rates/c/{currency}/")
     fun getCurrency(@Path("currency") currency: String): Observable<Currency>
 
+    /**
+     * Logic to create service by RetroFit Framework and connect to API
+     */
     companion object {
         fun create(): CurrencyService {
             val retrofit = Retrofit.Builder()
