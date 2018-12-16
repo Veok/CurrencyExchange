a2a507f387b30cff25e5fdc8317a6fbdc2d57271
veok <p.lelental@gmail.com>
2018-12-16 16:11:11 +0100

    Tests of internet connectivity

--- app/src/main/java/com/example/trebb/currencyexchange/CalculatorActivity.kt
+++ app/src/main/java/com/example/trebb/currencyexchange/CalculatorActivity.kt
@@ -1,7 +1,6 @@
 package com.example.trebb.currencyexchange
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 
 import kotlinx.android.synthetic.main.activity_calculator.*
@@ -9,6 +8,7 @@ import kotlinx.android.synthetic.main.activity_calculator.*
 class CalculatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
 
